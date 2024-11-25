6b9433861892cff5a3fd4a3fbecc1d5c694d34cf
Soumya Annie Thomas <soumyaannie07@gmail.com>
2024-11-25 21:34:53 +0530

    Day2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { Route } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Auth from './pages/Auth'
+import Footer from './components/Footer'
+
 
 function App() {
 
   return (
     <>
-      <h1>Project Fair</h1>
+
+      <Routes>
+        <Route path='/' element={<Home/> } />
+        <Route path='/login' element={<Auth/> } />
+        <Route path='/register' element={<Auth insideregister={true} /> } />
+        <Route path='/dashboard' element={<Dashboard/> } />
+        <Route path='/projects' element={<Projects/> } />
+      </Routes>
+      <Footer/>
     </>
   )
 }
